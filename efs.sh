afae074ffc68adea00b968551fd689e2bba66f55
howlcart <imannuelsmkn1bms@gmail.com>
2025-04-25 08:24:25 +0700

    update playbook

--- efs.sh
+++ efs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -xe
 sudo apt update
-sudo apt install nfs-common ansible -y
+sudo apt install -y nfs-common ansible unzip curl snapd
 ansible --version
-sudo mkdir /mnt/share
-sudo chmod -R 777 /mnt/share
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-05cacde14859cd25c.efs.us-east-1.amazonaws.com:/ /mnt/share
+sudo mkdir share
+sudo chmod -R 777 share
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-02995a7b915f6a691.efs.us-east-1.amazonaws.com:/ share
+sudo snap install amazon-ssm-agent --classic
+sudo systemctl enable --now snap.amazon-ssm-agent.amazon-ssm-agent.service
+sudo systemctl status snap.amazon-ssm-agent.amazon-ssm-agent.service
+
